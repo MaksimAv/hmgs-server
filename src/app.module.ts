92ef9714fe2aaab99f3a45eecbe2b5e28309ed14
Maksim Av. <maksim.av99@gmail.com>
2025-02-21 23:59:01 +0300

    refactor(filter): path, status code

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { RoomModule } from './modules/room/room.module';
 import { CORE_LAYER } from './core.layer';
 import { APP_FILTER } from '@nestjs/core';
-import { ZodExceptionFilter } from './shared/app-filters/zod.exception.filter';
+import { ZodExceptionFilter } from './shared/filters/zod.exception.filter';
 import { RoomCategoryModule } from './modules/room-category/room.category.module';
 
 @Module({
